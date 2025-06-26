aadc1569a0a48166120a7f0a3433afe5ecb7abe9
Serey Vatanak <45728734+vatanak10@users.noreply.github.com>
2025-06-26 15:01:14 +0700

    feat: implement chi

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,15 +17,24 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
 
-	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	return mux
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux http.Handler) error {
 
 	srv := &http.Server{
 		Addr:         app.config.addr,
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vatanak10/portfolio-backend
 
 go 1.23.5
+
+require github.com/go-chi/chi/v5 v5.2.2 // indirect
